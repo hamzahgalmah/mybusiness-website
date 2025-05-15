1b1664d724be8d208267349fa0b17f31f9d917da
hamzahgalmah <galmah45@gmail.com>
2025-05-15 13:57:38 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1 style={{ color: '#6b21a8' }}>Welcome to My Business Website</h1>
+      <p>This is your React + Vite + Tailwind starter.</p>
+    </div>
+  );
+}
+
+export default App;
